c9eecbb042baf05b691dfb96bbd917e5fb34ee20
Yunting <tsnoy23694549@gmail.com>
2018-09-03 14:56:58 +0800

    backup0903

--- app/src/main/java/com/example/user/myapplication/setting_setup.java
+++ app/src/main/java/com/example/user/myapplication/setting_setup.java
@@ -1,6 +1,10 @@
 package com.example.user.myapplication;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuInflater;
@@ -15,8 +19,6 @@ public class setting_setup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_setup);
-
-
     }
 
     //跳出選單
@@ -45,4 +47,6 @@ public class setting_setup extends AppCompatActivity {
             }
         });
     }
+
 }
+
