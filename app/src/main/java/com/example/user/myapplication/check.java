7f223c0b975951e80f4d0844f35c9bfc65464796
eunice <eunice29548164@gmail.com>
2018-09-28 12:47:39 +0800

    usage db

--- app/src/main/java/com/example/user/myapplication/check.java
+++ app/src/main/java/com/example/user/myapplication/check.java
@@ -18,20 +18,5 @@ public class check extends AppCompatActivity {
         sleep = findViewById(R.id.sleep);
         wake = findViewById(R.id.wake);
 
-        ListView listview = findViewById(R.id.list);
-        String[] str = {"AI", "NORMAL"};
-        ArrayAdapter adapter = new ArrayAdapter(this,
-                R.layout.check_item, R.id.alarm_type, str);
-        listview.setAdapter(adapter);
-
-        String[] date = {"20180928", "20180927"};
-        ArrayAdapter adapter1 = new ArrayAdapter(this,
-                R.layout.check_item, R.id.date, date);
-        listview.setAdapter(adapter1);
-
-        String[] time = {"0000000000000000", "00000000000000000"};
-        ArrayAdapter adapter2 = new ArrayAdapter(this,
-                R.layout.check_item, R.id.time, time);
-        listview.setAdapter(adapter2);
     }
 }
