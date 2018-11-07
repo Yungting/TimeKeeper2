f8587f06626b26f10bb513f0ee5e182511b57072
Yunting <tsnoy23694549@gmail.com>
2018-11-07 12:00:49 +0800

    修改ai manage

--- app/src/main/java/com/example/user/myapplication/ai_manage/ai_manage.java
+++ app/src/main/java/com/example/user/myapplication/ai_manage/ai_manage.java
@@ -1,120 +1,32 @@
 package com.example.user.myapplication.ai_manage;
 
-import android.content.Context;
-import android.os.IBinder;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.example.user.myapplication.R;
-import com.example.user.myapplication.setting_setup.CardPagerAdapter;
 
-public class ai_manage extends AppCompatActivity{
-
-//    private android.support.design.widget.TabLayout mTabs;
+public class ai_manage extends AppCompatActivity {
     private ViewPager mViewPager;
-    private CardAdapter mCardAdapter;
-//    private ai_manage_traffic traffic = new ai_manage_traffic();
-//    private ai_manage_holiday holiday = new ai_manage_holiday();
+
+    private CardPagerAdapter mCardAdapter;
+    private ShadowTransformer mCardShadowTransformer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ai_alarm_manage);
-
-        mViewPager = findViewById(R.id.viewpager);
-
+        mViewPager = findViewById(R.id.mviewPager);
 
         mCardAdapter = new CardPagerAdapter();
+        mCardAdapter.addCardItem(new CardItem("TYPHOONNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN", R.drawable.typhoon));
+        mCardAdapter.addCardItem(new CardItem("HOLIDAYYYYYYYYYYYYYYYYYYYYYYYYY", R.drawable.holiday));
 
-//        mCardAdapter = new CardAdapter(getChildManager(),
-////                dpToPixels(2, getActivity()));
+        mCardShadowTransformer = new ShadowTransformer(mViewPager, mCardAdapter);
 
-        mFragmentCardShadowTransformer = new ShadowTransformer(mViewPager, mFragmentCardAdapter);
-        mViewPager.setAdapter(mFragmentCardAdapter);
-        mViewPager.setPageTransformer(false, mFragmentCardShadowTransformer);
+        mViewPager.setAdapter(mCardAdapter);
+        mViewPager.setPageTransformer(false, mCardShadowTransformer);
         mViewPager.setOffscreenPageLimit(3);
-        mFragmentCardShadowTransformer.enableScaling(true);
-
-    }
-
-    public static float dpToPixels(int dp, Context context) {
-        return dp * (context.getResources().getDisplayMetrics().density);
-    }
-
-
-//        //fragment換頁
-//        mTabs = findViewById(R.id.tabs);
-
-//
-//        mTabs.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-//        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabs));
-//
-//        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
-//            @Override
-//            public Fragment getItem(int position) {
-//                switch (position) {
-//                    case 0:
-//                        return traffic;
-//                    case 1:
-//                        return holiday;
-//                }
-//                return null;
-//            }
-//
-//            @Override
-//            public int getCount() {
-//                return 2;
-//            }
-//
-//        });
-
-    }
-
-    //點擊空白處隱藏鍵盤
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-
-            // 获得当前得到焦点的View，一般情况下就是EditText（特殊情况就是轨迹求或者实体案件会移动焦点）
-            View v = getCurrentFocus();
-
-            if (isShouldHideInput(v, ev)) {
-                hideSoftInput(v.getWindowToken());
-            }
-        }
-        return super.dispatchTouchEvent(ev);
-    }
-
-    private boolean isShouldHideInput(View v, MotionEvent event) {
-        if (v != null && (v instanceof EditText)) {
-            int[] l = { 0, 0 };
-            v.getLocationInWindow(l);
-            int left = l[0], top = l[1], bottom = top + v.getHeight(), right = left
-                    + v.getWidth();
-            if (event.getX() > left && event.getX() < right
-                    && event.getY() > top && event.getY() < bottom) {
-                // 点击EditText的事件，忽略它。
-                return false;
-            } else {
-                return true;
-            }
-        }
-        // 如果焦点不是EditText则忽略，这个发生在视图刚绘制完，第一个焦点不在EditView上，和用户用轨迹球选择其他的焦点
-        return false;
-    }
-
-    private void hideSoftInput(IBinder token) {
-        if (token != null) {
-            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            im.hideSoftInputFromWindow(token, InputMethodManager.HIDE_NOT_ALWAYS);
-        }
+        mCardShadowTransformer.enableScaling(true);
     }
 }
